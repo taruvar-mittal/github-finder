c6edfeab6c016ad33840d3d65c444bcd23813148
taruvar-mittal <mittaltaruvar@gmail.com>
2020-10-08 21:16:02 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+async componentDidMount(){
+  // console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+  this.setState({ loading: true });
+
+  const res = await axios
+       .get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  this.setState({ users: res.data, loading: false});  
+       
+}
 
   render() {
 
@@ -11,7 +28,8 @@ class App extends Component {
       <div className="App">
        <Navbar />
        <div className="container">
-           <Users />
+          <Search />
+           <Users loading={this.state.loading} users={this.state.users} />
        </div>
       </div> 
     );
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-
-
-
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
+const UserItem = ({user: { login, avatar_url, html_url }}) => {
+        
         return (
             <div className="card text-center">
               <img src={avatar_url}
@@ -24,8 +21,11 @@ class UserItem extends Component {
                    </a>
                </div>
             </div>
-        )
-    }
-}
+        );
+};
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
